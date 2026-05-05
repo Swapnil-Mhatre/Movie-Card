9748533bbd526fa038a9074b5721944ec28c8d0e
Swapnil-Mhatre <swapmhatre889@gmail.com>
2026-05-05 14:45:00 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css"
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import GlobalState from "./context/context.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <StrictMode>
       <GlobalState>
         <App />
       </GlobalState>
     </StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
